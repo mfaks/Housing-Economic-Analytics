a40c33d88cb290927bf35c8257bbfaeb2559dd23
mfaks <muhammad.faks@gmail.com>
2025-09-13 10:08:04 -0700

    fix: updated zip file paths to use correct path structure

--- infra/storage.tf
+++ infra/storage.tf
@@ -2,14 +2,15 @@ resource "google_storage_bucket" "code_bucket" {
   name     = "${var.gcp_project}-function-code"
   location = var.gcp_region
 }
+
 resource "google_storage_bucket_object" "fred_zip" {
   name   = "fred.zip"
   bucket = google_storage_bucket.code_bucket.name
-  source = "../scripts/fred_function.zip"
+  source = "${path.module}/fred_function.zip"
 }
 
 resource "google_storage_bucket_object" "census_zip" {
   name   = "census.zip"
   bucket = google_storage_bucket.code_bucket.name
-  source = "../scripts/census_function.zip"
+  source = "${path.module}/census_function.zip"
 }
